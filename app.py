295c69f5772dfecdb537d85a23538ca30d84740c
hekt <hektorg@gmail.com>
2012-10-14 01:28:58 +0900

    Disable debug-mode

--- app.py
+++ app.py
@@ -5,17 +5,20 @@ from flask import Flask, Response, request, redirect, url_for, render_template
 
 app = Flask(__name__)
 
+
 def repHref(base_url, domain, match_obj):
     url = match_obj.group(2)
     if re.match(domain, url):
         return "".join([match_obj.group(1), base_url, url, match_obj.group(3)])
     return match_obj.group(0)
 
+
 def repUrl(url, match_obj):
     if not re.match("mailto:", match_obj.group(2)):
         return '%s="%s"' % (match_obj.group(1),
                             repRelPathToAbsPath(match_obj.group(2), url))
     return match_obj.group(0)
+
 
 def repRelPathToAbsPath(path, base_url):
     if re.match("http(s)?://", path):
@@ -24,8 +27,9 @@ def repRelPathToAbsPath(path, base_url):
         upper_depth = upperDepth(path)
         upper_path = upperDirectory(base_url, upper_depth)
         lower_path = re.sub("(/)?[\./]+/", '', path)
-        
+
         return "%s/%s" % (upper_path, lower_path)
+
 
 def upperDepth(relative_path):
     cnt = 0
@@ -34,8 +38,10 @@ def upperDepth(relative_path):
             cnt += (len(s) - 1)
     return cnt
 
+
 def upperDirectory(url, depth):
     return '/'.join(url.split('/')[:-(depth + 1)])
+
 
 @app.route('/ja/<path:url>')
 def autoEncodeJa(url):
@@ -52,18 +58,20 @@ def autoEncodeJa(url):
             pass
     else:
         encode = 'ascii'
-    
+
     return redirect(url_for('encodeJa', encoding=encoding, url=url))
+
 
 @app.route('/<encoding>/<path:url>')
 def encodeJa(encoding, url):
     if url.find(request.url_root) != -1:
         return 'invalid request'
-    
+
     base_url = "%s%s/" % (request.url_root, str(encoding))
     domain = '/'.join(url.split('/')[:3])
-    
-    url_exp = re.compile("(href|src|action)=[\"']((?:/)?(?:\.+/)*[^\"']*?)[\"']", re.I)
+
+    url_exp = re.compile(("(href|src|action)="
+                          "[\"']((?:/)?(?:\.+/)*[^\"']*?)[\"']"), re.I)
     href_exp = re.compile("(<a.*?href=[\"'])([^\"']*?)([\"'].*?>)", re.I)
     rep_func_url = lambda x: repUrl(url=url, match_obj=x)
     rep_func_href = lambda x: repHref(base_url=base_url, domain=domain,
@@ -76,11 +84,12 @@ def encodeJa(encoding, url):
 
     return Response(output, content_type=content_type)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.debug = True
     app.run(host="0.0.0.0", port=port)
